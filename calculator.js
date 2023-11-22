a8c55609d26b2bf4e83ad54d3d348ef5981bf340
Christopher Bogs <cbogs@upenn.edu>
2023-11-21 23:03:16 -0500

    Test calc functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,28 @@
 "use strict";
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+console.log ("10+5 equals: " + add(10, 5));
+console.log ("10-5 equals: " + subtract(10,5));
+console.log ("10*5 equals: " + multiply(10,5));
+console.log ("10/5 equals: " + divide(10,5));
+
+
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
