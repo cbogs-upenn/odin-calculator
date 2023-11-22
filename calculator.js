8570aefdd9cd19541d3bd991b8e0f8d59397dd28
Christopher Bogs <cbogs@upenn.edu>
2023-11-22 13:18:45 -0500

    Implement calculate()

--- calculator.js
+++ calculator.js
@@ -4,6 +4,28 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 
+console.log (calculate("*", 2, 3));
+
+
+function calculate(operator, firstNumber, secondNumber){
+    switch(operator){
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+        
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
